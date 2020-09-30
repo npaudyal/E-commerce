d2f7957197c58ed19a0a11deba53ea46df3e327c
npaudyal <zlatan.nis111@gmail.com>
2020-09-30 15:03:53 -0500

    Edit and Delete features

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const path = require('path');
-const errorController = require('./controllers/error')
 
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+
 const app = express();
-
-
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -13,7 +13,7 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
--- routes/shop.js
+++ routes/shop.js
@@ -10,15 +10,16 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
-router.get('/cart', shopController.getCart)
+router.get('/products/:productId', shopController.getProduct);
 
-router.post('/cart', shopController.postCart)
+router.get('/cart', shopController.getCart);
 
+router.post('/cart', shopController.postCart);
 
-router.get('/orders', shopController.getOrders)
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
-router.get('/products/:productId', shopController.getProduct)
+router.get('/orders', shopController.getOrders);
 
-router.get('/checkout', shopController.getCheckout)
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
